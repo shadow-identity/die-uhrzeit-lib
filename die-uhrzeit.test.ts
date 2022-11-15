fe936d5eef7e07ff5f5a10dad21c02ff45b6b209
Pavel Nedrigailov <pavel.nedr@gmail.com>
2022-11-15 21:18:44 +0100

    Return only string result (zwölf Uhr) + fixes

--- die-uhrzeit.test.ts
+++ die-uhrzeit.test.ts
@@ -1,18 +1,19 @@
-import { get12HourTime, getUhrzeitSimple } from "./die-uhrzeit"
+import { get12HourTime, getUhrzeitSimple, numberToGerman } from './die-uhrzeit'
 
-// jest test cases
-describe("getUhrzeitSimple", () => {
+describe('getUhrzeitSimple', () => {
 	test.each([
-		[new Date(2020, 0, 1, 0, 0), "12 Uhr"],
-		[new Date(2020, 0, 1, 0, 1), "1 nach 12"],
-		[new Date(2020, 0, 1, 0, 15), "viertel nach 12"],
-		[new Date(2020, 0, 1, 0, 30), "halb 1"],
-		[new Date(2020, 0, 1, 0, 45), "viertel vor 1"],
-		[new Date(2020, 0, 1, 0, 59), "1 vor 1"],
+		[new Date(2020, 0, 1, 0, 0), 'zwölf Uhr'],
+		[new Date(2020, 0, 1, 0, 1), 'eins nach zwölf'],
+		[new Date(2020, 0, 1, 0, 15), 'viertel nach zwölf'],
+		[new Date(2020, 0, 1, 0, 18), 'achtzehn nach zwölf'],
+		[new Date(2020, 0, 1, 0, 30), 'halb eins'],
+		[new Date(2020, 0, 1, 0, 35), 'fünfundzwanzig vor eins'],
+		[new Date(2020, 0, 1, 0, 45), 'viertel vor eins'],
+		[new Date(2020, 0, 1, 0, 59), 'eins vor eins'],
 	])('converts %p', (time, expected) => expect(getUhrzeitSimple(time)).toEqual(expected))
 })
 
-describe("get12HourTime", () => {
+describe('get12HourTime', () => {
 	test.each([
 		[0, 12],
 		[1, 1],
@@ -22,4 +23,24 @@ describe("get12HourTime", () => {
 		[24, 12],
 		[25, 1],
 	])('converts %i H', (raw, expected) => { expect(get12HourTime(raw)).toEqual(expected) })
+})
+
+describe('numberToGerman', () => {
+	test.each([
+		[1, 'eins'],
+		[9, 'neun'],
+		[10, 'zehn'],
+		[11, 'elf'],
+		[20, 'zwanzig'],
+		[21, 'einundzwanzig'],
+		[22, 'zweiundzwanzig'],
+		[29, 'neunundzwanzig'],
+		[30, 'dreißig'],
+	])('converts %i', (raw, expected) => { expect(numberToGerman(raw)).toEqual(expected) })
+
+	it('should return string with the same number and add a console error for 100', () => {
+		const spy = jest.spyOn(console, 'error')
+		expect(numberToGerman(100)).toEqual('100')
+		expect(spy).toHaveBeenCalledWith('Number must be less than 100')
+	})
 })
--- die-uhrzeit.ts
+++ die-uhrzeit.ts
@@ -5,33 +5,62 @@ type GetUhrzeit = (time: Date) => string
 export const get12HourTime = (hours: number): number => hours % 12 || 12
 
 export const getUhrzeitSimple: GetUhrzeit = (time) => {
-	// return informal german time string based on "time" property
-	// e.g. "3 nach 4" or "viertel vor 5"
+	// return informal german time string based on 'time' property
+	// e.g. '3 nach 4' or 'viertel vor 5'
 
 	const hour = get12HourTime(time.getHours())
 	const nextHour = get12HourTime(hour + 1)
 	const minutes = time.getMinutes()
 
+	const hourStr = numberToGerman(hour)
+	const nextHourStr = numberToGerman(nextHour)
+	const minutesStr = numberToGerman(minutes)
+
 	const min = minutes % 15
 
 	if (minutes === 0) {
-		return `${hour} Uhr`
+		return `${hourStr} Uhr`
 	} else if (minutes === 15) {
-		return `viertel nach ${hour}`
+		return `viertel nach ${hourStr}`
 	} else if (minutes === 30) {
-		return `halb ${nextHour}`
+		return `halb ${nextHourStr}`
 	} else if (minutes === 45) {
-		return `viertel vor ${get12HourTime(nextHour)}`
+		return `viertel vor ${nextHourStr}`
 	} else if (minutes < 30) {
-		return `${minutes} nach ${hour}`
+		return `${minutesStr} nach ${hourStr}`
 	} else {
-		return `${60 - minutes} vor ${get12HourTime(nextHour)}`
+		return `${numberToGerman(60 - minutes)} vor ${nextHourStr}`
 	}
 }
 
-// 	} else if (minutes <= 20) {
-// return `${minutes} nach ${hour}`
-// 	} else if (minutes <= 30) {
-// 	return `${minutes} vor halb ${hour + 1}`
-// } else if (minutes <= 40) {
-// 	return `${minutes} nach halb ${hour + 1}`
+export const getUhrzeitMedium: GetUhrzeit = (time) => { return 'WIP' }
+
+
+/*
+convert numbers from 1 to 59 to german string
+e.g. 1 => 'eins'
+e.g. 15 => 'fünfzehn'
+e.g. 59 => 'neunundfünfzig'
+*/
+export const numberToGerman = (number: number): string => {
+	if (number >= 100) {
+		console.error('Number must be less than 100')
+		return number.toString()
+	}
+
+	const ones = ['eins', 'zwei', 'drei', 'vier', 'fünf', 'sechs', 'sieben', 'acht', 'neun']
+	const teens = ['zehn', 'elf', 'zwölf', 'dreizehn', 'vierzehn', 'fünfzehn', 'sechzehn', 'siebzehn', 'achtzehn', 'neunzehn']
+	const tens = ['zehn', 'zwanzig', 'dreißig', 'vierzig', 'fünfzig']
+
+	if (number < 10) {
+		return ones[number - 1]
+	} else if (number < 20) {
+		return teens[number - 10]
+	} else if (number % 10 === 0) {
+		return tens[Math.floor(number / 10) - 1]
+	} else if (number.toString().slice(-1) === '1') {
+		return `einund${tens[Math.floor(number / 10) - 1]}`
+	} else {
+		return `${ones[number % 10 - 1]}und${tens[Math.floor(number / 10) - 1]}`
+	}
+}
